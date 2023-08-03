1740d90045db8c736832f66eeb0cb58f85f9c045
Тайсумова Оксана <119331948+Oksana2211@users.noreply.github.com>
2023-08-03 23:58:09 +0300

    correcrion styles

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -3,11 +3,19 @@ import st from "./style.module.scss"
 export default function Item(props) {
   console.log(props)
   return(
-    <section className={st.card}>
+    <> 
+    {props.item.flag ? (<section className={st.card1}>
     <div className={st[props.item.color]}><h2 className={st.name}>{props.item.name}</h2></div>
     <div className={st[props.item.color]}><h2 className={st.price}>{props.item.price}</h2></div>
     <div ><h3 className={st.speed}>{props.item.speed}</h3></div>
     <div ><h3 className={st.info}>{props.item.info}</h3></div>
-    </section>
+    </section>) : (<section className={st.card}>
+    <div className={st[props.item.color]}><h2 className={st.name}>{props.item.name}</h2></div>
+    <div className={st[props.item.color]}><h2 className={st.price}>{props.item.price}</h2></div>
+    <div ><h3 className={st.speed}>{props.item.speed}</h3></div>
+    <div ><h3 className={st.info}>{props.item.info}</h3></div>
+    </section>)}
+    
+    </>
   )
 }
