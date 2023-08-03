0fc28e962a42af5c3b050107892fae2b264b2111
Тайсумова Оксана <119331948+Oksana2211@users.noreply.github.com>
2023-08-03 23:34:42 +0300

    correction style

--- src/components/item/item.jsx
+++ src/components/item/item.jsx
@@ -4,10 +4,10 @@ export default function Item(props) {
   console.log(props)
   return(
     <section className={st.card}>
-    <h2 className={st.name}>{props.item.name}</h2>
-    <h2 className={st.price}>{props.item.price}</h2>
-    <div className={st.speed}>{props.item.speed}</div>
-    <div className={st.info}>{props.item.info}</div>
+    <div className={st[props.item.color]}><h2 className={st.name}>{props.item.name}</h2></div>
+    <div className={st[props.item.color]}><h2 className={st.price}>{props.item.price}</h2></div>
+    <div ><h3 className={st.speed}>{props.item.speed}</h3></div>
+    <div ><h3 className={st.info}>{props.item.info}</h3></div>
     </section>
   )
 }
